97ffa4a2fa66770bd9478f3ba39b8557ca952a01
Dmitriy <d.nikola1704@gmail.com>
2023-09-30 19:38:33 +0300

    updateTaskController

--- src/main/java/app/taskLogic/TaskController.java
+++ src/main/java/app/taskLogic/TaskController.java
@@ -1,73 +1,57 @@
 package app.taskLogic;
 
-import app.auth.models.User;
-
 import app.auth.payload.response.MessageResponse;
-import app.auth.repository.UserRepository;
 import app.taskLogic.models.Task;
+import app.taskLogic.request.AddTaskRequest;
 import app.taskLogic.request.UpdateTaskRequest;
 import app.taskLogic.service.TaskService;
-import app.taskLogic.service.*;
-import app.taskLogic.request.AddTaskRequest;
 import app.auth.security.jwt.JwtUtils;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
-import java.util.Optional;
 
+
+/*
+ * {
+ * "description":
+ * "id": получается из токена
+ * }
+ *
+ * */
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
     @Autowired
-    TaskService taskService;
+    private TaskService taskService;
+
     @Autowired
-    JwtUtils jwtUtils = new JwtUtils();
-    @Autowired
-    UserRepository userRepository;
+    private JwtUtils jwtUtils;
 
     @CrossOrigin(origins = "*")
     @PostMapping("/createTask")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<?> createTask(
-
-            /*
-             * {
-             * "description":
-             * "id": получается из токена
-             * }
-             *
-             * */
-
-
             @Valid @RequestBody AddTaskRequest addTaskRequest,
             @RequestHeader(value = "Authorization") String authorizationHeader) {
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String jwtToken = authorizationHeader.replace("Bearer ", "");
-            String description = addTaskRequest.getDescription();
-            Task task = new Task();
-            task.setDescription(addTaskRequest.getDescription());
-            task.setUserId(jwtUtils.getUserIdFromJwtToken(jwtToken));
-            Task createdTask = taskService.createTask(task);
 
-            return ResponseEntity.ok(createdTask);
+        if (!(authorizationHeader != null && authorizationHeader.startsWith("Bearer "))) {
+            return ResponseEntity.badRequest().body("Вы не авторизованы");
         }
-        return null;
-    }
 
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        Task task = new Task();
+        task.setDescription(addTaskRequest.getDescription());
+        task.setUserId(jwtUtils.getUserIdFromJwtToken(jwtToken));
+        Task createdTask = taskService.createTask(task);
+
+        return ResponseEntity.ok(createdTask);
+    }
 
     @CrossOrigin(origins = "*")
     @DeleteMapping("/deleteTask/{taskId}")
@@ -75,37 +59,23 @@ public class TaskController {
     public ResponseEntity<?> deleteTask(
             @PathVariable Long taskId,
             @RequestHeader(value = "Authorization") String authorizationHeader) {
-        {
-            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-                String jwtToken = authorizationHeader.replace("Bearer ", "");
-                Long userIdFromToken = jwtUtils.getUserIdFromJwtToken(jwtToken);
-                Long userIdByTaskId = taskService.getUserIdByTaskId(taskId);
-                if (Objects.equals(userIdFromToken, userIdByTaskId))
-                {
-                    taskService.deleteTask(taskId);
-                    return ResponseEntity.ok("Задача успешно удалена");
-                }
-                else {
-                    return ResponseEntity
-                            .badRequest()
-                            .body(new MessageResponse("Задача не найдена"));
-                }
-            }
 
-            else {
-                return ResponseEntity.badRequest().body("Вы не авторизованы");
-            }
+        if (!(authorizationHeader != null && authorizationHeader.startsWith("Bearer "))) {
+            return ResponseEntity.badRequest().body("Вы не авторизованы");
+        }
 
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        Long userIdFromToken = jwtUtils.getUserIdFromJwtToken(jwtToken);
+        Long userIdByTaskId = taskService.getUserIdByTaskId(taskId);
 
+        if (Objects.equals(userIdFromToken, userIdByTaskId)) {
+            taskService.deleteTask(taskId);
+            return ResponseEntity.ok("Задача успешно удалена");
+        } else {
+            return ResponseEntity.badRequest().body(new MessageResponse("Задача не найдена"));
         }
     }
 
-    /*
-    * {
-    * "description":
-    * }
-    *
-    * */
     @CrossOrigin(origins = "*")
     @PostMapping("/updateTask/{taskId}")
     @PreAuthorize("hasRole('USER')")
@@ -113,28 +83,27 @@ public class TaskController {
             @PathVariable Long taskId,
             @Valid @RequestBody UpdateTaskRequest updateTaskRequest,
             @RequestHeader(value = "Authorization") String authorizationHeader) {
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String jwtToken = authorizationHeader.replace("Bearer ", "");
-            Long userIdFromToken = jwtUtils.getUserIdFromJwtToken(jwtToken);
-            Long userIdByTaskId = taskService.getUserIdByTaskId(taskId);
 
-            if (Objects.equals(userIdFromToken, userIdByTaskId)) {
-                Task taskToUpdate = taskService.getTaskById(taskId).orElse(null);
-
-                if (taskToUpdate != null) {
-                    taskToUpdate.setDescription(updateTaskRequest.getDescription());
-                    taskService.updateTask(taskToUpdate);
-                    return ResponseEntity.ok("Задача успешно обновлена");
-                } else {
-                    return ResponseEntity
-                            .badRequest().body("Задача не найдена");
-                }
-            } else {
-                return ResponseEntity.badRequest().body("У вас нет прав на обновление этой задачи");
-            }
-        } else {
+        if (!(authorizationHeader != null && authorizationHeader.startsWith("Bearer "))) {
             return ResponseEntity.badRequest().body("Вы не авторизованы");
         }
-    }
 
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        Long userIdFromToken = jwtUtils.getUserIdFromJwtToken(jwtToken);
+        Long userIdByTaskId = taskService.getUserIdByTaskId(taskId);
+
+        if (Objects.equals(userIdFromToken, userIdByTaskId)) {
+            Task taskToUpdate = taskService.getTaskById(taskId).orElse(null);
+
+            if (taskToUpdate != null) {
+                taskToUpdate.setDescription(updateTaskRequest.getDescription());
+                taskService.updateTask(taskToUpdate);
+                return ResponseEntity.ok("Задача успешно обновлена");
+            } else {
+                return ResponseEntity.badRequest().body("Задача не найдена");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("У вас нет прав на обновление этой задачи");
+        }
+    }
 }
